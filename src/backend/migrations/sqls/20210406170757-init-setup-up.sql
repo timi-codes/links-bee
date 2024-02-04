aeb1f178cbe026b8926ae3596840c511138e2cad
ttejumola <tejumoladavid@gmail.com>
2024-02-03 23:38:38 -0500

    implement user and link schema

--- src/backend/migrations/sqls/20210406170757-init-setup-up.sql
+++ src/backend/migrations/sqls/20210406170757-init-setup-up.sql
@@ -1,6 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA IF NOT EXISTS test;
 
+CREATE TABLE IF NOT EXISTS "user" (
+    id uuid DEFAULT uuid_generate_v4(),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    last_login TIMESTAMP DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
+
 CREATE TABLE IF NOT EXISTS "link" (
     bee_id VARCHAR (7) NOT NULL,
     name VARCHAR (255),
@@ -11,14 +21,5 @@ CREATE TABLE IF NOT EXISTS "link" (
     updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
     user_id uuid,
     PRIMARY KEY (bee_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
-);
-
-
-CREATE TABLE IF NOT EXISTS "user" (
-    id uuid DEFAULT uuid_generate_v4(),
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (email)
+    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
 );
