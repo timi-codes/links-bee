76de207e3bc4bfc61a8e0715e96e1357721c8c0f
ttejumola <tejumoladavid@gmail.com>
2024-02-03 23:25:35 -0500

    implement user and link schema

--- src/backend/migrations/sqls/20210406170757-init-setup-up.sql
+++ src/backend/migrations/sqls/20210406170757-init-setup-up.sql
@@ -3,17 +3,22 @@ CREATE SCHEMA IF NOT EXISTS test;
 
 CREATE TABLE IF NOT EXISTS "link" (
     bee_id VARCHAR (7) NOT NULL,
+    name VARCHAR (255),
     original_url VARCHAR (255) NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     last_visited_at TIMESTAMP,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
-    PRIMARY KEY (bee_id)
-    UNIQUE (original_url, bee_id)
+    user_id uuid,
+    PRIMARY KEY (bee_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
 );
 
 
 CREATE TABLE IF NOT EXISTS "user" (
     id uuid DEFAULT uuid_generate_v4(),
     email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
